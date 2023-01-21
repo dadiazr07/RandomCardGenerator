f02e94ef2be679963f82c1f852683fc9dd54005e
dadiazr07 <dadiazr.07@gmail.com>
2023-01-21 00:50:07 +0000

    CartaCSS + JS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+let iconos = ["♦", "♥", "♠", "♣"];
+let valorCarta = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let colorCarta = ["red", "black"];
+
+let iconoRandom = Math.floor(Math.random() * iconos.length + 1);
+let valorRandom = Math.floor(Math.random() * valorCarta.length + 1);
+let colorRandom = Math.floor(Math.random() * colorCarta.length + 1);
+
+console.log(iconoRandom);
+console.log(valorRandom);
+console.log(colorRandom);
