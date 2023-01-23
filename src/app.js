4e389d1d130a63402fae60a1eece5d334ea558c8
dadiazr07 <dadiazr.07@gmail.com>
2023-01-23 22:04:18 +0000

    JS simplificado

--- src/app.js
+++ src/app.js
@@ -26,59 +26,19 @@ let valorCarta = [
   "Q",
   "K"
 ];
-let player = ["", "", ""];
-
-// // Funcion paga generar valores e iconos aleatorios
+let player = [];
 
 function genCards(Array1, Array2) {
-  let carta1 = ["", ""];
-  let carta2 = ["", ""];
-  let carta3 = ["", ""];
-
-  function iconoRandom(Array1) {
-    let pinta = Array1[Math.floor(Math.random() * Array1.length)];
-    return pinta;
+  let genericCard = [];
+  for (let i = 0; i < 3; i++) {
+    genericCard[0] = Array1[Math.floor(Math.random() * Array1.length)];
+    genericCard[1] = Array2[Math.floor(Math.random() * Array2.length)];
+    player[i] = genericCard;
+    genericCard = [];
   }
-
-  function valorRandom(Array2) {
-    let numeroCarta = Array2[Math.floor(Math.random() * Array2.length)];
-    return numeroCarta;
-  }
-
-  function card1() {
-    let pinta1 = iconoRandom(Array1);
-    let numeroCarta = valorRandom(Array2);
-    carta1[0] = pinta1;
-    carta1[1] = numeroCarta;
-    player[0] = carta1;
-    numeroCarta = "";
-  }
-
-  function card2() {
-    let pinta2 = iconoRandom(Array1);
-    let numeroCarta = valorRandom(Array2);
-    carta2[0] = pinta2;
-    carta2[1] = numeroCarta;
-    player[1] = carta2;
-    numeroCarta = "";
-  }
-
-  function card3() {
-    let pinta3 = iconoRandom(Array1);
-    let numeroCarta = valorRandom(Array2);
-    carta3[0] = pinta3;
-    carta3[1] = numeroCarta;
-    player[2] = carta3;
-    numeroCarta = "";
-  }
-
-  card1();
-  card2();
-  card3();
-  return player;
 }
 
-// Para chckear la funcion en el inspector
+// Para checkear la funcion en el inspector
 // genCards(iconos, valorCarta);
 // console.log(player);
 
