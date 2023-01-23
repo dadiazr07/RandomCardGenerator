db60bcf1b05b5359d6552bdfb82fc97f4f360706
dadiazr07 <dadiazr.07@gmail.com>
2023-01-23 23:16:53 +0000

    JS Simplified (animation + random cards)

--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-};
-
 document.getElementById("shuffle").addEventListener("click", shuffle);
 
 let iconos = ["♦", "♥", "♠", "♣"];
@@ -34,6 +30,7 @@ function genCards(Array1, Array2) {
   for (let i = 0; i < 3; i++) {
     let pinta = Array1[Math.floor(Math.random() * Array1.length)];
     let numValue = Array2[Math.floor(Math.random() * Array2.length)];
+
     genericCard[0] = pinta;
     genericCard[1] = numValue;
     player[i] = genericCard;
@@ -47,7 +44,6 @@ function genCards(Array1, Array2) {
 
 function shuffle() {
   genCards(iconos, valorCarta);
-  console.log(player);
 
   let iconsCarta1 = document.getElementsByClassName("iconoCar1");
   iconsCarta1[0].innerHTML = player[0][0];
@@ -64,20 +60,15 @@ function shuffle() {
   iconsCarta3[1].innerHTML = player[2][0];
   document.getElementById("numberCar3").innerHTML = player[2][1];
 
-  let ICarta1 = document.getElementById("carta1");
-  let ICarta2 = document.getElementById("carta2");
-  let ICarta3 = document.getElementById("carta3");
-  ICarta1.style.animation = "none";
-  ICarta1.offsetHeight;
-  ICarta1.style.animation = null;
-
-  ICarta2.style.animation = "none";
-  ICarta2.offsetHeight;
-  ICarta2.style.animation = null;
-
-  ICarta3.style.animation = "none";
-  ICarta3.offsetHeight;
-  ICarta3.style.animation = null;
+  // Actualizar y activar animacion
+  let elements = ["carta1", "carta2", "carta3"].map(id =>
+    document.getElementById(id)
+  );
+  elements.forEach(element => {
+    element.style.animation = "none";
+    element.offsetHeight;
+    element.style.animation = null;
+  });
 
   // // Esto resetea Player array
   player = [];
