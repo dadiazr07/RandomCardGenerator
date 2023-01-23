04646bfe80adb5ce66fcb8d096cdc86e54bfd34e
dadiazr07 <dadiazr.07@gmail.com>
2023-01-23 22:51:09 +0000

    JS CSS Animation in Function

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  document.getElementById("shuffle").addEventListener("click", shuffle);
 };
+
+document.getElementById("shuffle").addEventListener("click", shuffle);
 
 let iconos = ["♦", "♥", "♠", "♣"];
 let valorCarta = [
@@ -32,9 +33,9 @@ function genCards(Array1, Array2) {
   let genericCard = [];
   for (let i = 0; i < 3; i++) {
     let pinta = Array1[Math.floor(Math.random() * Array1.length)];
-    let valCart = Array2[Math.floor(Math.random() * Array2.length)];
+    let numValue = Array2[Math.floor(Math.random() * Array2.length)];
     genericCard[0] = pinta;
-    genericCard[1] = valCart;
+    genericCard[1] = numValue;
     player[i] = genericCard;
     genericCard = [];
   }
@@ -48,27 +49,36 @@ function shuffle() {
   genCards(iconos, valorCarta);
   console.log(player);
 
-  let iconsCarta1 = document
-    .getElementById("carta1")
-    .getElementsByClassName("iconoCar1");
+  let iconsCarta1 = document.getElementsByClassName("iconoCar1");
   iconsCarta1[0].innerHTML = player[0][0];
   iconsCarta1[1].innerHTML = player[0][0];
   document.getElementById("numberCar1").innerHTML = player[0][1];
 
-  let iconsCarta2 = document
-    .getElementById("carta2")
-    .getElementsByClassName("iconoCar2");
+  let iconsCarta2 = document.getElementsByClassName("iconoCar2");
   iconsCarta2[0].innerHTML = player[1][0];
   iconsCarta2[1].innerHTML = player[1][0];
   document.getElementById("numberCar2").innerHTML = player[1][1];
 
-  let iconsCarta3 = document
-    .getElementById("carta3")
-    .getElementsByClassName("iconoCar3");
+  let iconsCarta3 = document.getElementsByClassName("iconoCar3");
   iconsCarta3[0].innerHTML = player[2][0];
   iconsCarta3[1].innerHTML = player[2][0];
   document.getElementById("numberCar3").innerHTML = player[2][1];
 
+  let ICarta1 = document.getElementById("carta1");
+  let ICarta2 = document.getElementById("carta2");
+  let ICarta3 = document.getElementById("carta3");
+  ICarta1.style.animation = "none";
+  ICarta1.offsetHeight;
+  ICarta1.style.animation = null;
+
+  ICarta2.style.animation = "none";
+  ICarta2.offsetHeight;
+  ICarta2.style.animation = null;
+
+  ICarta3.style.animation = "none";
+  ICarta3.offsetHeight;
+  ICarta3.style.animation = null;
+
   // // Esto resetea Player array
-  player = ["", "", ""];
+  player = [];
 }
